806f27c1fe4be9388f4ac4ff214c3acf524d06bf
Valery Golovin <valery.golovin@medm.com>
2022-02-23 22:45:01 +0300

    a lot

--- src/intArraySet/set.java
+++ src/intArraySet/set.java
@@ -1,5 +1,6 @@
 package intArraySet;
 
+
 public class set {
     private class position{
         int index;
@@ -36,6 +37,10 @@ public class set {
 
     }
 
+    public set(){
+        new set(-100,100);
+    }
+
     public void printData(){
         System.out.println("Shift: " + shift);
         System.out.println("Zero at: " + zeroPosition);
@@ -49,16 +54,146 @@ public class set {
     public void insert(int q){
         if (q < start || q > end) return;
         position p = findInArray(q);
-        if (isTaken(p)) return;
 
-        int mask = -1 << 31;
-        mask >>>= p.pos;
-        array[p.index] += mask;
+        array[p.index] |= 0b10000000000000000000000000000000 >>> p.pos;
+    }
+
+    public void delete(int q){
+        if (q < start || q > end) return;
+        position p = findInArray(q);
+
+        array[p.index] &= ~(0b10000000000000000000000000000000 >>> p.pos);
+    }
+
+    public void assign(set a){
+        new set(a.start, a.end);
+        shift = a.shift;
+        zeroPosition = a.zeroPosition;
+
+        for (int i = 0; i < a.array.length; i ++){
+            array[i] = a.array[i];
+        }
+    }
+
+    public int min(){
+        for (int i = 0; i < array.length; i++){
+            if (array[i] != 0){
+                int mask;
+                for (int j = 0; j < 32; j++){
+                    mask = 0b10000000000000000000000000000000 >> j;
+                    if ((array[i] & mask) != 0){
+                        return (32 * i - shift + j );
+                    }
+                }
+
+            }
+        }
+        throw new myException("The set is empty");
+    }
+
+    public int max(){
+        for (int i = array.length - 1; i >= 0; i--){
+            if (array[i] != 0){
+                int mask;
+                int maskCounter = 0;
+                for (int j = 31; j >= 0; j--){
+                    mask =  1 << maskCounter;
+                    if ((array[i] & mask) != 0){
+                        return (32 * i + j);
+                    }
+                    maskCounter++;
+                }
+
+            }
+        }
+        throw new myException("The set is empty");
+    }
+
+    public boolean equal(set a){
+        if (end < a.start || a.end < start)
+            return false;
+
+        //equal sets
+        if (start == a.start && end == a.end){
+            for (int i = 0; i < array.length; i++)
+                if (array[i] != a.array[i])
+                    return false;
+
+            return true;
+        }
+
+        //one set - other's subset
+        if ((start > a.start && end < a.end) || (a.start > start && a.end < end)){
+            set biggerSet, smallerSet;
+            if (a.start > start){
+                biggerSet = this;
+                smallerSet = a;
+            }
+            else {
+                biggerSet = a;
+                smallerSet = this;
+            }
+
+            int intersectionStart = findInArray(smallerSet.start).index;
+            int intersectionEnd = findInArray(smallerSet.end).index;
+
+            for (int i = 0; i < intersectionStart; i++){
+                if (biggerSet.array[i] != 0)
+                    return false;
+            }
+
+            int counter = 0;
+            for (int i = intersectionStart; i < intersectionEnd; i++){
+                if (biggerSet.array[i] != smallerSet.array[counter])
+                    return false;
+
+                counter++;
+            }
+
+            for (int i = intersectionEnd; i < biggerSet.array.length; i++){
+                if (biggerSet.array[i] != 0)
+                    return false
+            }
+
+            return true;
+        }
+
+        //more variants
+        return false;
+    }
+
+    public void makeNull(){
+        for (int i = 0; i < array.length; i++){
+            array[i] = 0;
+        }
+    }
+
+    public boolean member(int q){
+        if (q < start || q > end) return false;
+        position p = findInArray(q);
+        return isTaken(p);
+    }
+
+    public boolean ifIntersect(set a){
+        if (a.end < start || a.start > end) return false;
+
+        int intersectionStart = Math.max(a.start, start);
+        int intersectionEnd = Math.min(a.end, end);
+
+        int firstSetStart = findInArray(intersectionStart).index;
+        int secondSetStart = a.findInArray(intersectionStart).index;
+
+
+        for (int i = intersectionStart; i <= intersectionEnd; i++){
+            if (array[firstSetStart] - array[secondSetStart] != 0)
+            //how to compare
+        }
+
+        return true;
     }
 
     private boolean isTaken(position q){
-        int mask = -1 << 31;
-        mask >>>=  q.pos;
+        int mask = 0b10000000000000000000000000000000 >>> q.pos;
         return !((array[q.index] & mask) == 0);
     }
 
--- src/intArraySet/test.java
+++ src/intArraySet/test.java
@@ -4,10 +4,16 @@ public class test {
     public static void main(String[] args) {
         set s1 = new set(0, 31);
         s1.printData();
-        s1.insert(31);
+        s1.insert(28);
         s1.printData();
-        s1.insert(30);
+        s1.insert(3);
         s1.printData();
-        
+
+
+        System.out.println();
+        System.out.println(s1.min());
+        System.out.println(s1.max());
+
+        System.out.println(s1.member(28));
     }
 }
